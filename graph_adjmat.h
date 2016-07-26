43663935f7e7019c3d66a9d49ae044f7b361315b
TianLanhe <849416763@qq.com>
2016-07-26 13:19:30 +0800

    图(邻接矩阵)头文件

--- graph_adjmat.h
+++ graph_adjmat.h
@@ -94,3 +94,4 @@ Status FloShortPath(MGraph graph,int path[MAXVEX][MAXVEX][MAXVEX+2],int dist[MAX
 //根据Floyd算法求图graph中每个顶点到其余顶点的距离(到自身距离为0，无法到达距离为MAXINT)
 //path[v][w][...]存储点v到w的最短路径，其中path[v][w][0]表示最短路径的顶点个数(包括起点与终点)，接下来n个元素为顶点。dist[v][w]表示顶点v到w的距离
 #endif
+
