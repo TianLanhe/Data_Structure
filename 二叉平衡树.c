27bb215c2fee9aa3189ec367a007a8057ca839a9
振宇 <849416763@qq.com>
2016-08-26 23:35:42 +0800

    动态查找——二叉平衡树

--- 二叉平衡树.c
+++ 二叉平衡树.c
@@ -26,8 +26,15 @@ Status Right_Balance(BiTNode **root);
 Status InsertAVL(BiTNode **root,int target,Status *taller);
 //向二叉排序树中插入关键字为target的节点。若已存在target关键字的节点，则插入失败，返回ERROR
 //若不存在target，则在相应位置插入节点，并进行旋转(若需要)，返回OK
+Status DeleteAVL_sub(BiTNode **root,int target,Status *shorter,Status *flag);
+//删除二叉排序树中关键字为target的节点，若不存在target关键字的节点，则删除失败，返回ERROR
+//若存在target，则在删除该节点并调整节点位置与平衡因子，返回OK
+Status DeleteAVL(BiTNode **root,int target,Status *shorter);
+//封装了上述的删除函数，隐藏了flag参数
 BiTNode *SearchAVL(BiTNode *root,int target);
 //搜索二叉平衡树，若存在关键字为target的节点，则返回该节点的地址，否则返回NULL
+Status isAVL(BiTNode *root);
+//判断该数是否是二叉平衡树，是则返回true，否则返回false
 
 Status InOrderTraverse(BiTNode *root,Status (*visit)(BiTNode *)){
 	if(root){
@@ -169,21 +176,130 @@ Status InsertAVL(BiTNode **root,int target,Status *taller){
 		return OK;
 	}
 }
+Status DeleteAVL_sub(BiTNode **root,int target,Status *shorter,Status *flag){
+	if(*root == NULL){//没找到结点，删除失败//二叉平衡树删除操作基本思路：
+		*shorter=false;						//找到该节点，若没有子树或只有一边子树
+		return ERROR;						//则删除该节点，并往上回溯修改bf与旋转
+	}else if((*root)->data == target){		//若该节点有两边子树，比较复杂，首先与
+		if(*flag == true){//第二次到达顶点	//左边的右尽头节点交换关键值，然后继续
+			BiTNode *temp;					//往左，当第二次找到该关键值的节点时，
+			*shorter=true;					//删除该节点，并接上左子树，再往上回溯
+			temp=*root;						//调整节点
+			*root=(*root)->lchild;	//接上左子树，可以证明不会影响左子树的bf
+			free(temp);				//删除该节点
+			return OK;
+		}else{
+			BiTNode *temp;
+			int tmp;
+			*flag=true;
+			if((*root)->lchild == NULL){
+				*shorter=true;
+				temp=*root;
+				*root=(*root)->rchild;
+				free(temp);
+				return OK;
+			}else if((*root)->rchild == NULL){	//只有左子树或者右子树时，删除该节点即可
+				*shorter=true;
+				temp=*root;
+				*root=(*root)->lchild;
+				free(temp);
+				return OK;
+			}else{
+				temp=(*root)->lchild;
+				while(temp->rchild)temp=temp->rchild;
+				tmp=temp->data;							//交换数据
+				temp->data=(*root)->data;
+				(*root)->data=tmp;						//往左边继续搜索，一定搜索得到
+				if(DeleteAVL_sub(&(*root)->lchild,target,shorter,flag) == ERROR)return ERROR;
+				if(*shorter == true){
+					switch((*root)->bf){
+					case 1:*shorter=true;
+							(*root)->bf--;
+							break;
+					case 0:*shorter=false;
+							(*root)->bf--;
+							break;
+					case -1:(*root)->bf--;
+							if(Right_Balance(root) == ERROR)return ERROR;
+							*shorter=false;
+							break;
+					default:return ERROR;
+					} 
+				}
+			}
+		}
+	}else if(target < (*root)->data){
+		if(DeleteAVL_sub(&(*root)->lchild,target,shorter,flag) == ERROR)return ERROR;
+		if(*shorter == true){
+			switch((*root)->bf){
+			case 1:*shorter=true;
+					(*root)->bf--;
+					break;
+			case 0:*shorter=false;
+					(*root)->bf--;
+					break;
+			case -1:(*root)->bf--;
+					if(Right_Balance(root) == ERROR)return ERROR;
+					*shorter=false;
+					break;
+			default:return ERROR;
+			} 
+		}
+	}else if(target > (*root)->data){
+		if(DeleteAVL_sub(&(*root)->rchild,target,shorter,flag) == ERROR)return ERROR;
+		if(*shorter == true){
+			switch((*root)->bf){
+			case -1:*shorter=true;
+					(*root)->bf++;
+					break;
+			case 0:*shorter=false;
+					(*root)->bf++;
+					break;
+			case 1:(*root)->bf++;
+					if(Left_Balance(root) == ERROR)return ERROR;
+					*shorter=false;
+					break;
+			default:return ERROR;
+			} 
+		}
+	}
+	return OK;
+}
+Status DeleteAVL(BiTNode **root,int target,Status *shorter){
+	int flag=false;
+	return DeleteAVL_sub(root,target,shorter,&flag);
+}
 BiTNode *SearchAVL(BiTNode *root,int target){
 	if(root == NULL)return NULL;
 	else if(root->data == target)return root;
 	else if(target < root->data)return SearchAVL(root->lchild,target);
 	else return SearchAVL(root->rchild,target);
 }
-
+Status isAVL(BiTNode *root){
+	if(root == NULL)return true;
+	else{
+		if(root->bf > 1 || root->bf < -1)return false;
+		if(root->lchild && root->lchild->data >= root->data)return false;
+		if(root->rchild && root->rchild->data <= root->data)return false;
+		if(isAVL(root->lchild) == false)return false;
+		if(isAVL(root->rchild) == false)return false;
+		return true;
+	}
+}
 int main(){
 	BiTNode *root=NULL;
 	int a[10]={62,88,58,47,35,73,51,99,37,93};
 	int i;
-	int flag;
+	int flag=false;
+	int shorter;
 	for(i=0;i<10;i++)
-		if(InsertAVL(&root,a[i],&flag) == ERROR)printf("ERROR\n");
+		if(InsertAVL(&root,a[i],&shorter) == ERROR)printf("ERROR\n");
 	InOrderTraverse(root,PrintBiTree);
 	printf("\n");
+	printf("%d\n",isAVL(root));
+	if(DeleteAVL(&root,93,&shorter) == ERROR)printf("ERROR\n");
+	InOrderTraverse(root,PrintBiTree);
+	printf("\n");
+	printf("%d\n",isAVL(root));
 	return 0;
 }
