a4a5f811ff6d0964468c14e7b9f72f9e79b0c0bc
ZhenyuHe <849416763@qq.com>
2018-06-07 14:59:25 +0800

    添加数独随机生成的代码

--- stack/applyment_of_recursion/sudoku.c
+++ stack/applyment_of_recursion/sudoku.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define N 9
 #define SQUARE 3
 #define true 1
 #define false 0
 #define bool int
+#define FILL ' '
 
 void input(char a[N][N]);
 //输入待填充数独 
@@ -20,7 +23,13 @@ bool checkMap(char a[N][N]);
 void next(int* i, int *j);
 //求数独下一个位置 
 void solve(char a[N][N], int i, int j);
-//求解数独 
+//求解数独
+bool generate(char a[N][N]);
+//初始化棋面为空格，然后生成数独
+bool generateSudoku(char a[N][N], int i, int j);
+//生成数独
+int getRandomNum(int start, int len);
+//随机生成[start,start+len)之间的随机数
 
 void input(char a[N][N]) {
 	int i, j;
@@ -52,7 +61,7 @@ bool checkColumn(char a[N][N]) {
 		for (i = 0; i < N; ++i) {
 			if (flag[a[i][j] - '0'] == true)
 				return false;
-			if (a[i][j] != ' ')
+			if (a[i][j] != FILL)
 				flag[a[i][j] - '0'] = true;
 		}
 	}
@@ -66,7 +75,7 @@ bool checkRow(char a[N][N]) {
 		for (j = 0; j < N; ++j) {
 			if (flag[a[i][j] - '0'] == true)
 				return false;
-			if (a[i][j] != ' ')
+			if (a[i][j] != FILL)
 				flag[a[i][j] - '0'] = true;
 		}
 	}
@@ -83,7 +92,7 @@ bool checkSquare(char a[N][N]) {
 				for (l = j; l < SQUARE + j; ++l) {
 					if (flag[a[k][l] - '0'] == true)
 						return false;
-					if (a[k][l] != ' ')
+					if (a[k][l] != FILL)
 						flag[a[k][l] - '0'] = true;
 				}
 			}
@@ -109,7 +118,7 @@ void solve(char a[N][N], int i, int j) {
 		output(a);
 		return;
 	}
-	else if (a[i][j] != ' ') {
+	else if (a[i][j] != FILL) {
 		next(&i, &j);
 		solve(a, i, j);
 	}
@@ -124,14 +133,61 @@ void solve(char a[N][N], int i, int j) {
 				next(&_i, &_j);
 				solve(a, _i, _j);
 			}
-			a[i][j] = ' ';
+			a[i][j] = FILL;
 		}
 	}
+}
+
+int getRandomNum(int start, int len) {
+	int r;
+	int bucket_size = RAND_MAX / len;
+	do {
+		r = rand() / bucket_size;
+	} while (r >= len);
+	return r + start;
+}
+
+bool generateSudoku(char a[N][N], int i, int j) {
+	if (i == N)
+		return true;
+
+	int n, x;
+	int flag[N + 1] = { 0 };
+	int _i, _j;
+	for (n = 0; n < N; ++n) {
+		do {
+			x = getRandomNum(1, 9);
+		} while (flag[x]);
+		flag[x] = true;
+
+		a[i][j] = x + '0';
+
+		if (checkMap(a)) {
+			_i = i;
+			_j = j;
+			next(&_i, &_j);
+			if (generateSudoku(a, _i, _j))
+				return true;
+		}
+	}
+	a[i][j] = ' ';
+	return false;
+}
+
+bool generate(char a[N][N]) {
+	int i, j;
+	for (i = 0; i < N; ++i)
+		for (j = 0; j < N; ++j)
+			a[i][j] = ' ';
+	return generateSudoku(a, 0, 0);
 }
 
 int main() {
 	char map[N][N];
 	input(map);
+	//srand(time(NULL));
+	//generate(map);
+	//output(map);
 	solve(map, 0, 0);
 	return 0;
 }
